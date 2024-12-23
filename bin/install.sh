8fefa96a81e38477701d0d8ecb36425540d33214
LazyBST <ayusharora17785@gmail.com>
2024-12-23 18:02:33 +0530

    chore: updated install scirpt

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Variables
-INSTALL_DIR="/usr/local/bin"
-BINARY_URL="binary_name"
+INSTALL_DIR="/tmp"
+BINARY_URL="https://github.com/LazyBST/KMAgent/blob/main/bin/kmagent-linux-arm"
 BINARY_NAME="kmagent-linux-arm"
 CONFIG_FILE_PATH_TARGET="/tmp/otelcol.yaml"
 CONFIG_SERVICE_ORIGIN_URL="http://localhost:3000"
 
-curl -L "$BINARY_URL" -o "$INSTALL_DIR $BINARY_NAME"
+curl -L "$BINARY_URL" -o "$INSTALL_DIR"
 
-chmod +x "$INSTALL_DIR $BINARY_NAME"
+chmod +x "$INSTALL_DIR/$BINARY_NAME"
 
-"$INSTALL_DIR $BINARY_NAME" -configFilePath="$CONFIG_FILE_PATH_TARGET" -configServiceOriginUrl="$CONFIG_SERVICE_ORIGIN_URL"
+"$INSTALL_DIR/$BINARY_NAME" -configFilePath="$CONFIG_FILE_PATH_TARGET" -configServiceOriginUrl="$CONFIG_SERVICE_ORIGIN_URL"
