7b202c050287e137e8e4025acb6b3e5d4a1e6a9e
LazyBST <ayusharora17785@gmail.com>
2024-12-31 13:44:13 +0530

    feat: updated install script

--- bin/install.sh
+++ bin/install.sh
@@ -2,12 +2,12 @@
 
 # Variables
 INSTALL_DIR="/tmp"
-BINARY_URL="https://github.com/LazyBST/KMAgent/blob/main/bin/kmagent-linux-arm"
-BINARY_NAME="kmagent-linux-arm"
+BINARY_URL="https://raw.githubusercontent.com/LazyBST/KMAgent/refs/heads/main/bin/kmagent-darwin-arm64"
+BINARY_NAME="kmagent-darwin-arm64"
 CONFIG_FILE_PATH_TARGET="/tmp/otelcol.yaml"
 CONFIG_SERVICE_ORIGIN_URL="http://localhost:3000"
 
-curl -L "$BINARY_URL" -o "$INSTALL_DIR"
+curl -L "$BINARY_URL" -o "$INSTALL_DIR/$BINARY_NAME"
 
 chmod +x "$INSTALL_DIR/$BINARY_NAME"
 
